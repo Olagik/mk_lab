14133a0ffb6f3bd73c96c3545b23c1ffd8da0aff
Olagik <lapaevoleg99@gmail.com>
2021-04-22 13:37:33 +0500

    init

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ creators.addEventListener('click',() =>{
         document.querySelector('.creators').classList.add('btn_per_active')
         document.querySelector('.actors').classList.remove('btn_per_active')
 })
+
+
+
+document.querySelector('.burger').addEventListener('click',()=>{
+    console.log('dct gjikj');
+        document.querySelector('.burger-menu').classList.add("active");
+        document.querySelector('.header').classList.add("close-logo");
+        document.querySelector('.burger').classList.add("close-burger");
+        document.querySelector('.header').classList.remove("active-logo");
+        document.querySelector('.burger').classList.remove("active-burger");
+})
+document.querySelector('.material-icons-outlined').addEventListener('click',()=>{
+    document.querySelector('.burger-menu').classList.remove("active");
+    document.querySelector('.header').classList.add("active-logo");
+    document.querySelector('.burger').classList.add("active-burger");
+    document.querySelector('.header').classList.remove("close-logo");
+    document.querySelector('.burger').classList.remove("close-burger");
+})
